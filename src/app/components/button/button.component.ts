12707db3dc441a6a1435bb0725eb51430e257c17
GersonBenito <gersonbenito4@gmail.com>
2023-04-30 18:56:38 -0600

    feat: custom button component added

--- src/app/components/button/button.component.ts
+++ src/app/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 // agregar clases dinamicas
 // const getStyles = (...args: string[]) => ['button', ...args].filter(Boolean);
@@ -15,11 +15,18 @@ export class ButtonComponent {
   @Input() public label: string = 'Button';
   @Input() public type: 'primary' | 'secondary' | 'tertiary' = 'primary';
   @Input() public size: 'small' | 'medium' | 'large' = 'small';
+  @Input() public typeButton: 'submit' | 'button' = 'button';
+  @Output() public onClick = new EventEmitter<Event>();
+  // @Output() public onAction: EventEmitter<any> = new EventEmitter();
 
   // agregar clases dinamicas
   public get classes(): string[] {
     // return getStyles(this.type);
-    return ['button', this.type, this.size]
+    return ['button', this.type, this.size];
+  }
+
+  actionButton(){
+    this.onClick.emit();
   }
 
 }
--- src/stories/button/button.component.stories.ts
+++ src/stories/button/button.component.stories.ts
@@ -1,47 +1,55 @@
-import { Meta, StoryFn } from "@storybook/angular";
+import { Meta, StoryObj } from "@storybook/angular";
 import { ButtonComponent } from "src/app/components/button/button.component";
 
-export default {
+const template: Meta<ButtonComponent> = {
    title: 'Components/button',
+   tags: ['autodocs'],
    component: ButtonComponent,
-   args: {
-    label: 'Primary',
-    type: 'primary'
-   }
-} as Meta;
-
-const Template: StoryFn<ButtonComponent> = (args: ButtonComponent) => ({
-    props: args
-});
-
-export const Primary = Template.bind({});
-
-export const Secondary = Template.bind({});
-Secondary.args = {
-    label: 'Secondary',
-    type: 'secondary',
+   render: (args: ButtonComponent) => ({props: args}),
+   args: {}
 };
 
-export const Tertiary = Template.bind({});
-Tertiary.args = {
-    label: 'Tertiary',
-    type: 'tertiary'
+export default template;
+type Story = StoryObj<ButtonComponent>; 
+
+export const Primary: Story = {
+    args: {
+        label: 'Primary',
+        type: 'primary'
+    }
 }
 
-export const Small = Template.bind({});
-Small.args = {
-    label: 'Button',
-    size: 'small'
+export const Secondary: Story = {
+    args: {
+        label: 'Secondary',
+        type: 'secondary'
+    }
 }
 
-export const Medium = Template.bind({});
-Medium.args = {
-    label: 'Button',
-    size: 'medium'
+export const Tertiary: Story = {
+    args: {
+        label: 'Tertiary',
+        type: 'tertiary'
+    }
 }
 
-export const Large = Template.bind({});
-Large.args = {
-    label: 'Button',
-    size: 'large'
+export const Small: Story = {
+    args: {
+        label: 'Button',
+        size: 'small'
+    }
+}
+
+export const Medium: Story = {
+    args: {
+        label: 'Button',
+        size: 'medium'
+    }
+}
+
+export const Large: Story = {
+    args: {
+        label: 'Button',
+        size: 'large'
+    }
 }
